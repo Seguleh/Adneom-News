01e3589dff1b162b3d844cbe9c588018613738f7
Seguleh <aezhco@gmail.com>
2016-04-11 19:25:47 +0000

    Post AngularJS test

--- app/assets/javascripts/adneom_news.js
+++ app/assets/javascripts/adneom_news.js
@@ -1,6 +1,42 @@
 angular.module('AdneomNews', [])
-.controller('AppCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
+.factory('posts', [function(){
+}]);
+
+.controller('AppContrl', ['$scope',function($scope){
+
+		$scope.posts = [
+			{title: 'post 1', upvotes: 5},
+		  	{title: 'post 2', upvotes: 2},
+		  	{title: 'post 3', upvotes: 15},
+		  	{title: 'post 4', upvotes: 9},
+		  	{title: 'post 5', upvotes: 4}
+		];
+	};
+
+	$scope.addPost = function(){
+
+		if(!$scope.title || $scope.title === '') { return; }
+		if(!$scope.link || $scope.link === '') { return; }
+
+	  	$scope.posts.push({
+
+	  		title: $scope.title, 
+
+	  		link: $scope.link,
+
+	  		upvotes: 0
+
+	  	});
+
+	  	$scope.title = '';
+	  	$scope.link = '';
+
+	};
+
+	$scope.incrementUpvotes = function(post) {
+
+  		post.upvotes += 1;
+
+	};
+
 }]);
