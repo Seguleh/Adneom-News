ef033e00b86201a5793f7e268f11701841038978
Seguleh <aezhco@gmail.com>
2016-04-12 16:08:10 +0000

    Creating flash msgs

--- app/assets/javascripts/posts/postsContrl.js
+++ app/assets/javascripts/posts/postsContrl.js
@@ -10,8 +10,8 @@ angular.module('AdneomNews')
 
 		$scope.addComment = function() {
 
-		  if($scope.body === '') { $("#notice").append("<div id='alert-notice' class='alert alert-danger'>The Body field can't be empty<div>").fadeIn().fadeOut(3000, function() { $(this).removeChild(); }); return; }
-		  if($scope.author === '') { $("#notice").append("<div id='alert-notice' class='alert alert-danger'>The Author field can't be empty<div>").fadeIn().fadeOut(3000, function() { $(this).removeChild(); }); return; }
+		  if($scope.body === '') { $("#notice").show().append("<div id='alert-notice' class='alert alert-danger'>The Body field can't be empty<div>").fadeOut(3000, function() { $(this).removeChild(); }); return; }
+		  if($scope.author === '') { $("#notice").show().append("<div id='alert-notice' class='alert alert-danger'>The Author field can't be empty<div>").fadeOut(3000, function() { $(this).removeChild(); }); return; }
 
 		  posts.createComment(post.id, {
 
@@ -21,7 +21,7 @@ angular.module('AdneomNews')
 		  }).success(function(comment) {
 
 		  	$scope.post.comments.push(comment);
-		  	$("#notice").append("<div id='alert-notice' class='alert alert-success'>Comment created successfully!<div>").fadeIn().fadeOut(3000, function() { $(this).removeChild(); });
+		  	$("#notice").show().append("<div id='alert-notice' class='alert alert-success'>Comment created successfully!<div>").fadeOut(3000, function() { $(this).removeChild(); });
 
 		  });
 
