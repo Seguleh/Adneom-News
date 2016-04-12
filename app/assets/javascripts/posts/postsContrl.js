fd4d9634307ca50419634e1686bdb4637e1c63be
Seguleh <aezhco@gmail.com>
2016-04-12 14:25:52 +0000

    Creating flash msgs

--- app/assets/javascripts/posts/postsContrl.js
+++ app/assets/javascripts/posts/postsContrl.js
@@ -10,8 +10,8 @@ angular.module('AdneomNews')
 
 		$scope.addComment = function() {
 
-		  if($scope.body === '') { $("#notice").append("<div class='alert alert-danger'>The Body field can't be empty<div>").fadeOut(3000); return; }
-		  if($scope.author === '') { $("#notice").append("<div class='alert alert-danger'>The Author field can't be empty<div>").fadeOut(3000); return; }
+		  if($scope.body === '') { $("#notice").append("<div class='alert alert-danger'>The Body field can't be empty<div>").fadeOut(3000).remove(); return; }
+		  if($scope.author === '') { $("#notice").append("<div class='alert alert-danger'>The Author field can't be empty<div>").fadeOut(3000).remove(); return; }
 
 		  posts.createComment(post.id, {
 
@@ -21,7 +21,7 @@ angular.module('AdneomNews')
 		  }).success(function(comment) {
 
 		  	$scope.post.comments.push(comment);
-		  	$("#notice").append("<div class='alert alert-success'>Comment created successfully!<div>").fadeOut(3000);
+		  	$("#notice").append("<div class='alert alert-success'>Comment created successfully!<div>").fadeOut(3000).remove();
 
 		  });
 
