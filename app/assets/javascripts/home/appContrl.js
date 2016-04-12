92508f3e16c90b38eaadfbaebc797c85f507da77
Seguleh <aezhco@gmail.com>
2016-04-12 16:41:55 +0000

    Creating flash msgs

--- app/assets/javascripts/home/appContrl.js
+++ app/assets/javascripts/home/appContrl.js
@@ -10,8 +10,10 @@ angular.module('AdneomNews')
 	
 		$scope.addPost = function(){
 
-			if(!$scope.title || $scope.title === '') {$("#notice").append("<div id='alert' class='alert alert-danger'>The Title field can't be empty<div>").fadeOut(3000, function() { $("#alert").remove(); }); $(this).css({"display": "block", "opacity": "1"}); return; }
-			if(!$scope.link || $scope.link === '') {$("#notice").append("<div id='alert' class='alert alert-danger'>The Link field can't be empty<div>").fadeOut(3000, function() { $("#alert").remove(); }); $(this).css({"display": "block", "opacity": "1"}); return; }
+			$("#notice").css({"display": "block", "opacity": "1"});
+
+			if(!$scope.title || $scope.title === '') {$("#notice").append("<div id='alert' class='alert alert-danger'>The Title field can't be empty<div>").fadeOut(3000, function() { $("#alert").remove(); }); return; }
+			if(!$scope.link || $scope.link === '') {$("#notice").append("<div id='alert' class='alert alert-danger'>The Link field can't be empty<div>").fadeOut(3000, function() { $("#alert").remove(); }); return; }
 
 		  	posts.create({
 
@@ -20,7 +22,7 @@ angular.module('AdneomNews')
 
 		  	}).success(function (){
 
-		  		$("#notice").append("<div id='alert' class='alert alert-success'>Post created successfully!<div>").fadeOut(3000, function() { $("#alert").remove(); $(this).css({"display": "block", "opacity": "1"}); });
+		  		$("#notice").append("<div id='alert' class='alert alert-success'>Post created successfully!<div>").fadeOut(3000, function() { $("#alert").remove(); });
 
 		  	});
 
--- app/assets/javascripts/posts/postsContrl.js
+++ app/assets/javascripts/posts/postsContrl.js
@@ -10,8 +10,10 @@ angular.module('AdneomNews')
 
 		$scope.addComment = function() {
 
-		  if($scope.body === '') { $("#notice").append("<div id='alert' class='alert alert-danger'>The Body field can't be empty<div>").fadeOut(3000, function() { $("#alert").remove(); $(this).css({"display": "block", "opacity": "1"}); }); return; }
-		  if($scope.author === '') { $("#notice").append("<div id='alert' class='alert alert-danger'>The Author field can't be empty<div>").fadeOut(3000, function() { $("#alert").remove(); $(this).css({"display": "block", "opacity": "1"}); }); return; }
+		  $("#notice").css({"display": "block", "opacity": "1"});
+
+		  if($scope.body === '') { $("#notice").append("<div id='alert' class='alert alert-danger'>The Body field can't be empty<div>").fadeOut(3000, function() { $("#alert").remove(); }); return; }
+		  if($scope.author === '') { $("#notice").append("<div id='alert' class='alert alert-danger'>The Author field can't be empty<div>").fadeOut(3000, function() { $("#alert").remove(); }); return; }
 
 		  posts.createComment(post.id, {
 
@@ -21,7 +23,7 @@ angular.module('AdneomNews')
 		  }).success(function(comment) {
 
 		  	$scope.post.comments.push(comment);
-		  	$("#notice").append("<div id='alert' class='alert alert-success'>Comment created successfully!<div>").fadeOut(3000, function() { $("#alert").remove(); $(this).css({"display": "block", "opacity": "1"}); });
+		  	$("#notice").append("<div id='alert' class='alert alert-success'>Comment created successfully!<div>").fadeOut(3000, function() { $("#alert").remove(); });
 
 		  });
 
