68180dd6dc8c9ea8f91bdbf70a0821318a30bf15
Seguleh <aezhco@gmail.com>
2016-04-12 02:57:27 +0000

    Double Tree fix

--- app/assets/javascripts/adneom_news.js
+++ app/assets/javascripts/adneom_news.js
@@ -10,7 +10,7 @@ angular.module('AdneomNews', ['ui.router', 'templates'])
     $stateProvider
       .state('home', {
         url: '/home',
-        templateUrl: '_home.html.haml',
+        templateUrl: 'templates/_home.html.haml',
         controller: 'AppContrl',
         resolve: {
           postPromise: ['posts', function(posts){
@@ -18,10 +18,10 @@ angular.module('AdneomNews', ['ui.router', 'templates'])
             }]
         }
       })
-      
+
       .state('posts', {
         url: '/posts/{id}',
-        templateUrl: '_posts.html.haml',
+        templateUrl: 'templates/_posts.html.haml',
         controller: 'PostsContrl',
         resolve: {
           post: ['$stateParams', 'posts', function($stateParams, posts) {
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,3 @@
 //= require angular-rails-templates
 //= require angular-ui-router
 //= require_tree .
-//= require_tree ../templates
