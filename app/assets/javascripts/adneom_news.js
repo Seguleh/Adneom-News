ddd8fd83e2ac7394168b31a66a67b4e31f1df63e
Seguleh <aezhco@gmail.com>
2016-04-12 03:00:38 +0000

    Why Heroku why

--- app/assets/javascripts/adneom_news.js
+++ app/assets/javascripts/adneom_news.js
@@ -10,7 +10,7 @@ angular.module('AdneomNews', ['ui.router', 'templates'])
     $stateProvider
       .state('home', {
         url: '/home',
-        templateUrl: 'templates/_home.html.haml',
+        templateUrl: 'home/_home.html.haml',
         controller: 'AppContrl',
         resolve: {
           postPromise: ['posts', function(posts){
@@ -21,7 +21,7 @@ angular.module('AdneomNews', ['ui.router', 'templates'])
 
       .state('posts', {
         url: '/posts/{id}',
-        templateUrl: 'templates/_posts.html.haml',
+        templateUrl: 'posts/_posts.html.haml',
         controller: 'PostsContrl',
         resolve: {
           post: ['$stateParams', 'posts', function($stateParams, posts) {
