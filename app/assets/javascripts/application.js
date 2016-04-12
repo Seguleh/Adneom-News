9d6ca9bbfb469fd6866c56b3916365bf00736327
Seguleh <aezhco@gmail.com>
2016-04-12 02:44:48 +0000

    Placed files back to folders

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+//= require jquery
+//= require jquery_ujs
+//= require angular
+//= require angular-rails-templates
+//= require angular-ui-router
+//= require_tree .
+//= require_tree ../templates
---
+++ app/assets/javascripts/home/appContrl.js
@@ -0,0 +1,36 @@
+angular.module('AdneomNews')
+
+.controller('AppContrl', [
+
+	'$scope','posts',
+
+	function($scope){
+
+		$scope.posts = posts.posts;
+	
+
+		$scope.addPost = function(){
+
+			if(!$scope.title || $scope.title === '') { return; }
+			if(!$scope.link || $scope.link === '') { return; }
+
+		  	$posts.create({
+
+		  		title: $scope.title, 
+		  		link: $scope.link,
+		  		upvotes: 0,
+
+		  	});
+
+		  	$scope.title = '';
+		  	$scope.link = '';
+
+		};
+
+		$scope.incrementUpvotes = function(post){
+
+	  		posts.upvote(post);
+
+		};
+
+}]);
